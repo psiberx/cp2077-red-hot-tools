1fc07f78d04721bcced30dac4fe9c7e14bf6ddd1
pv <p@siberx.dev>
2024-03-01 00:00:42 +0300

    Support address library

--- src/App/Project.hpp
+++ src/App/Project.hpp
@@ -12,5 +12,5 @@ constexpr auto Author = "psiberx";
 constexpr auto NameW = L"RedHotTools";
 constexpr auto AuthorW = L"psiberx";
 
-constexpr auto Version = semver::from_string_noexcept("0.8.1").value();
+constexpr auto Version = semver::from_string_noexcept("0.9.0").value();
 }
